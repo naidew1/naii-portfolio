243d2e8502568ee9882d7e9530a9a8b234924228
naidew1 <nailha.dewi@student.president.ac.id>
2026-07-01 10:08:40 +0700

    fix: resolve unused import build error

--- src/constants/staticImage.ts
+++ src/constants/staticImage.ts
@@ -1,23 +1,12 @@
-import istriku from "/public/istriku.jpg";
-import highlightProject from "/public/highlight-project.png";
 import avatar from "/public/my-avatar.png";
 
 import avatar1 from "/public/web-placeholder.svg";
 import avatar2 from "/public/ctf-placeholder.svg";
 import avatar3 from "/public/ai-placeholder.svg";
 
-import projectOne from "/public/isone-blog.png";
-import projectTwo from "/public/ic-club.png";
-import projectTree from "/public/myprogress.png";
-
 export default {
   avatar1,
   avatar2,
   avatar3,
-  istriku,
   avatar,
-  highlightProject,
-  projectOne,
-  projectTwo,
-  projectTree,
 };
